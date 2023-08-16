2fd8fafbd9774c094e64e66526e13a89eb5173a7
jineo29 <david.programacion@gruporyc.com.mx>
2023-08-16 12:32:02 -0600

    update code

--- src/componentes/atomos/Header.jsx
+++ src/componentes/atomos/Header.jsx
@@ -1,12 +1,11 @@
-import '../../assets/styles/HeaderStyle.css'
-import Logo from "../../assets/images/LogoPeko.png"
-import { useState, useRef } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Modal from 'react-bootstrap/Modal';
+import "../../assets/styles/HeaderStyle.css";
+import Logo from "../../assets/images/LogoPeko.png";
+import { useState, useRef } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
-
 
 function Header() {
   const [show, setShow] = useState(false);
@@ -17,40 +16,58 @@ function Header() {
   const handlerClick = (e) => {
     e.preventDefault();
     const formData = new FormData(formdata.current);
-    const correo = formData.get('correo');
-    let URI = "http://localhost:3000/Usuarios";
-    let options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        Nombre: formData.get("name"),
-        Telefono: formData.get("telefono"),
-        Correo: formData.get("correo"),
-      }),
-    };
-    fetch(URI, options)
+    const correo = formData.get("correo");
+    fetch("http://localhost:3000/Usuarios")
       .then((response) => response.json())
-      .then((data) => {
-        if (data.message === "Usuario registrado exitosamente") {
-          Swal.fire({
-            position: "top-end",
-            width: 200,
-            icon: "success",
-            title: "Usuario Registrado",
-            text: `Correo registrado: ${correo}`,
-            showConfirmButton: false,
-            timer: 1500,
-          }).then(() => {
-            setShow(false);
-          });
-        } else {
+      .then((correosExistentes) => {
+        const listaCorreos = correosExistentes.map((correos) => correos.Correo);
+
+        if (listaCorreos.includes(correo)) {
           Swal.fire({
             icon: "error",
             title: "Error al registrar usuario",
-            text: data.error,
+            text: `El correo  ${correo}  ya está en uso.`,
           });
+        } else {
+          const URI = "http://localhost:3000/Usuarios";
+          const opciones = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              Nombre: formData.get("name"),
+              Telefono: formData.get("telefono"),
+              Correo: correo,
+            }),
+          };
+
+          fetch(URI, opciones)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.message === "Usuario registrado exitosamente") {
+                Swal.fire({
+                  position: "top-end",
+                  width: 200,
+                  icon: "success",
+                  title: "Usuario Registrado",
+                  text: `Correo registrado: ${correo}`,
+                  showConfirmButton: false,
+                  timer: 1500,
+                }).then(() => {
+                  setShow(false);
+                });
+              } else {
+                Swal.fire({
+                  icon: "error",
+                  title: "Error al registrar usuario",
+                  text: data.error,
+                });
+              }
+            })
+            .catch((error) => {
+              console.error("Error:", error);
+            });
         }
       })
       .catch((error) => {
@@ -60,16 +77,23 @@ function Header() {
   return (
     <>
       <div className="navbar">
-        <div className='logo'>
+        <div className="logo">
           <img src={Logo} alt="logo" />
           <h1>Peko</h1>
         </div>
-
         <div>
-          <Link to="/" className='sinlinea'><button className='boton'>HOME</button></Link>
-          <Link to="/card" className='sinlinea'><button className='boton'>CONOCENOS</button></Link>
-          <a src="#footer" className='sinlinea'><button className='boton'>CONTACTO</button></a>
-          <button onClick={handleShow} className='boton'>REGISTRAR</button>
+          <Link to="/" className="sinlinea">
+            <button className="boton">HOME</button>
+          </Link>
+          <Link to="/card" className="sinlinea">
+            <button className="boton">CONOCENOS</button>
+          </Link>
+          <a src="#footer" className="sinlinea">
+            <button className="boton">CONTACTO</button>
+          </a>
+          <button onClick={handleShow} className="boton">
+            REGISTRAR
+          </button>
         </div>
       </div>
 
@@ -88,7 +112,7 @@ function Header() {
                 name="name"
               />
               <Form.Control
-               type="telephone"
+                type="telephone"
                 placeholder="Telefono"
                 autoFocus
                 name="telefono"
